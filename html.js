e93d48cd107670a77107dd66db35a24dcf1eae29
oktot <38215780+oktot@users.noreply.github.com>
2018-06-14 21:33:14 +0700

    Update html.js

--- html.js
+++ html.js
@@ -35,14 +35,14 @@ module.exports = React.createClass({
           <link rel="shortcut icon" href='/bakadono_favicon_32plus144@.png' />
           <link rel="apple-touch-icon" href='/bakadono_favicon_32plus144@.png' />
           <meta property="og:image" content='/bakadono-144@.png' />
-	  <meta name="google-site-verification" content=" " />
+	  <meta name="google-site-verification" content="B0mHdLXrxdeDg7FFQY1Jb1KF0jNWRtnMRdi6cDsQH-c" />
           <meta name="format-detection" content='telephone=no' />
           {head.meta.toComponent()}
         </head>
         <body>
           <div id="react-mount" style={{width:'100%'}} dangerouslySetInnerHTML={ {    __html: this.props.body} } />
           <script src={ prefixLink(`/bundle.js?t=${BUILD_TIME}`) } />
-          <script async src='https://www.googletagmanager.com/gtag/js?id=UA- ' />
+          <script async src='https://www.googletagmanager.com/gtag/js?id=UA-120797776-1' />
 		          { process.env.NODE_ENV === 'production' ? GoogleAnalyticsSetup : null }
         </body>
       </html>
@@ -68,7 +68,7 @@ function buildGoogleAnalyticsSetup() {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA- ');
+  gtag('config', 'UA-120797776-1');
   `;
 
   return <script
