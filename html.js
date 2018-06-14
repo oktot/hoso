2ec894281877a6dbfcbdf17ca343b9603cc6dce1
oktot <38215780+oktot@users.noreply.github.com>
2018-06-14 21:38:33 +0700

    Update html.js

--- html.js
+++ html.js
@@ -53,7 +53,7 @@ module.exports = React.createClass({
 function buildGoogleAdSense() {
   const js = `
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: 'ca-pub- ',
+    google_ad_client: 'ca-pub-6455147427493091',
     enable_page_level_ads: true
   });
   `;
