c0f7ecad5754364ca0870f0fb17a214bc5fd702f
oktot <38215780+oktot@users.noreply.github.com>
2019-01-28 20:31:48 +0700

    Update html.js

--- html.js
+++ html.js
@@ -35,7 +35,7 @@ module.exports = React.createClass({
           <link rel="shortcut icon" href='/bakadono_favicon_32plus144@.png' />
           <link rel="apple-touch-icon" href='/bakadono_favicon_32plus144@.png' />
           <meta property="og:image" content='/bakadono-144@.png' />
-	 <meta name="google-site-verification" content="09wL2KeGp_Ws2nfVN1hVLHd-2OkI" />
+	<meta name="google-site-verification" content="lkDUE89Tq0s1RXbiGS_jTkaZeT_8r7n8aSAYkQZ8ywY" />
           <meta name="format-detection" content='telephone=no' />
           {head.meta.toComponent()}
         </head>
